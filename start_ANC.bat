9b4fd9978fa1844f0505c59d9908fa3120b496a1
nemuilemon <mou09069489124@gmail.com>
2025-11-03 01:07:11 +0900

    fix stratup bat file encoding issue

--- start_ANC.bat
+++ start_ANC.bat
@@ -1,5 +1,6 @@
 @echo off
-echo ､｢､熙ｹ､ｯﾆｰ､ｷ､ﾆ､､､ﾞ､ｹ...
+chcp 932
+echo ありすを起動しています...
 call .\.venv\Scripts\activate.bat
-echo ｴﾄｶｭ､ﾎｽ猜ｬ､ﾇ､ｭ､ﾞ､ｷ､ｿ｡｣A.N.C.､ｯﾆｰ､ｷ､ﾞ､ｹ｡｣
+echo 環境の準備ができました。A.N.C.を起動します。
 python main.py
